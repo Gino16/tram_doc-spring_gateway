aa619ca7daf74dffe629951dad71fd4a4dcbbc2e
Gino Ascencio <gino.francisco@hotmail.com>
2020-12-26 19:46:11 -0500

    Spring Gateway

---
+++ src/test/java/com/tramite_documentario/microservicios/backend/spring_gateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tramite_documentario.microservicios.backend.spring_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
